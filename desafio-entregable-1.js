94564ac3599512e6c0ee811d04bc71a5cc4e9d56
tobilucero1 <tobilucero05@gmail.com>
2023-04-25 18:18:00 -0300

    first commit

---
+++ desafio-entregable-1.js
@@ -0,0 +1,26 @@
+class ProductManager{
+    constructor(title,description,price,thumbnail,code,stock){
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.thumbnail = thumbnail;
+        this.code = code;
+        this.stock = stock;
+    }
+
+addProduct(){
+
+
+}
+
+getProduct(){
+
+
+}
+
+getProductById(){
+
+
+}
+
+}
